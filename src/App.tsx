dd1115cb1a5a04c444099995916d5bf85241a63a
tami <tamiortami@gmail.com>
2023-12-05 15:02:30 +0900

    feat: button test

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,15 @@ function App() {
   };
   return (
     <div className="App">
-      <button onClick={fetchQuotesData}>Load Data</button>
+      <button
+        data-modal-target="default-modal"
+        data-modal-toggle="default-modal"
+        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+        onClick={fetchQuotesData}
+      >
+        Check today's quote
+      </button>
       {isLoading ? <p>Loading...</p> : <p>{data?.slip.advice}</p>}
     </div>
   );
