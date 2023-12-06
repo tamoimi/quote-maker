0852c9908af3c04e2f394672111382e2386583c4
tami <tamiortami@gmail.com>
2023-12-06 09:42:35 +0900

    style

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ function App() {
     queryClient.fetchQuery("/fetchQuotes");
   };
   return (
-    <div className="App">
+    <div className="flex flex-col gap-5 p-4 text-center">
       <button
         data-modal-target="default-modal"
         data-modal-toggle="default-modal"
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="block text-white bg-teal-700 hover:bg-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
         type="button"
         onClick={fetchQuotesData}
         // onClick={toggleModal}
@@ -46,15 +46,15 @@ function App() {
         //     aria-hidden="true"
         //     className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
         //   >
-        //     <div className="relative p-4 w-full max-w-2xl max-h-full">
+        //     <div className="relative w-full max-w-2xl max-h-full p-4">
         //       {/* <!-- Modal content --> */}
         //       <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
         //         {/* <!-- Modal header --> */}
-        //         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+        //         <div className="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
         //           <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Terms of Service</h3>
         //           <button
         //             type="button"
-        //             className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+        //             className="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
         //             data-modal-hide="default-modal"
         //           >
         //             <svg
@@ -76,7 +76,7 @@ function App() {
         //           </button>
         //         </div>
         //         {/* <!-- Modal body --> */}
-        //         <div className="p-4 md:p-5 space-y-4">
+        //         <div className="p-4 space-y-4 md:p-5">
         //           <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
         //             With less than a month to go before the European Union enacts new consumer privacy laws for its
         //             citizens, companies around the world are updating their terms of service agreements to comply.
